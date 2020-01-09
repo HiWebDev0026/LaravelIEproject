6d6954f371f77a2684e8ff5e74c294948e783098
HiAIDev <asahide0026@gmail.com>
2020-01-09 11:39:48 +0330

    Set Dashboard View

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,50 @@
-<h1>The dashboard </h1>
+@extends('layouts.master')
+
+@section('content')
+<section class="row new-post">
+    <div class="col-m-6 col-md-offset-3">
+        <header>
+            <h3>What do you have to say ?! </h3>
+        </header>
+        <form action="">
+            <div class="form-group">
+                <textarea class="form-control" name="new-post" id="new-post" rows="6"
+                    placeholder="Write something to post!"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Send post</button>
+        </form>
+    </div>
+</section>
+<section class="row posts">
+    <div class="col-md-6 col-md-offset-3">
+        <header>
+            <h3>Yours and others posts</h3>
+        </header>
+        <article class="post">
+            <p>Example of 1th post</p>
+            <div class="info">
+                Posted by me on 9 jan 2020
+            </div>
+            <div class="interaction">
+                <a href="#">Like</a> |
+                <a href="#">Dislike</a> |
+                <a href="#">Edit</a> |
+                <a href="#">Delete</a>
+            </div>
+        </article>
+        <article class="post">
+            <p>Example of 1th post</p>
+            <div class="info">
+                Posted by me on 9 jan 2020
+            </div>
+            <div class="interaction">
+                <a href="#">Like</a> |
+                <a href="#">Dislike</a> |
+                <a href="#">Edit</a> |
+                <a href="#">Delete</a>
+            </div>
+        </article>
+    </div>
+</section>
+
+@endsection
