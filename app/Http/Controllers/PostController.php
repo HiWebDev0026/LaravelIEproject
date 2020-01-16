88a0462788919f34d24a2ce55f2300ec134a3cb2
HiAIDev <asahide0026@gmail.com>
2020-01-16 09:50:19 +0330

    Users cant delete the post of other peoples

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Routing\Controller as cn;
 class PostController extends Controller
 {
     public function getDashboard(){
-        $posts = Post::all(); 
+        $posts = Post::orderBy('created_at', 'desc')->get(); 
         return view('dashboard', ['posts' => $posts]);
     }
     
@@ -30,6 +30,9 @@ class PostController extends Controller
     public function getDeletePost($post_id)
     {
         $post = Post::where('id', $post_id)->first();
+        if(Auth::user() != $post->user){
+            return redirect()->back();
+        }
         $post->delete();
         return redirect()->route('dashboard')->with(['message' => 'The post successfully deleted!']);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,9 +31,12 @@
             </div>
             <div class="interaction">
                 <a href="#">Like</a> |
-                <a href="#">Dislike</a> |
+                <a href="#">Dislike</a>
+                @if(Auth::user() == $post->user)
+                |
                 <a href="#">Edit</a> |
                 <a href="{{ route('post.delete', ['post_id' => $post->id]) }}">Delete</a>
+                @endif
             </div>
         </article>
         @endforeach
