cf904c94a3d7151000d538dfb682331c77690c42
HiAIDev <asahide0026@gmail.com>
2020-01-15 23:03:23 -0800

    add fallow button and edit search results page

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -1,12 +1,13 @@
 @extends('.layouts.master')
 
 @section('content')
-    <h3>Your search results for "{{ Request::input('search_query')}}" (email or id): </h3>
+    <h4><i>Your search results for "{{ Request::input('search_query')}}" (email or id): </i></h4>
     @if(!$search_results->count())
         <p>No results found, sorry:(</p>
     @else
     <div class="row">
         <div class="col-md-12">
+        <hr>
         @foreach($search_results as $search_result)
             @include('includes.user/partials/userblock')
         @endforeach
