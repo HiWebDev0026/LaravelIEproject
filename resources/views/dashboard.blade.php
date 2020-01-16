ec1a567a046da78eb3da1760a153e6a4ed9127b8
HiAIDev <asahide0026@gmail.com>
2020-01-16 08:03:35 -0800

    add feature to show modal when click on edit posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
     </div>
 </section>
 <section class="row posts">
-    <div class="col-md-6 col-md-offset-3">
+    <div class="col-md-12 col-md-offset-3">
         <header>
             <h5>Yours and others posts</h5>
         </header>
@@ -44,5 +44,21 @@
         @endforeach
     </div>
 </section>
-
+<div class="modal fade" tabindex="-1" role="dialog" id="edit-modal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Edit Post</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+        <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
@@ -16,5 +18,6 @@
         <div class="container">
             @yield('content')
         </div>
+        <script src="{{URL::to('src/js/app.js')}}"></script>
     </body>
 </html>
