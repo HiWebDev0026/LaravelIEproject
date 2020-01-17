8e9f6d21f0f4c80629e1321aa7581316567a1db3
HiAIDev <asahide0026@gmail.com>
2020-01-17 19:18:31 +0330

    changed buttons and attributes

--- resources/views/includes/user/partials/userrequests.blade.php
+++ resources/views/includes/user/partials/userrequests.blade.php
@@ -7,9 +7,15 @@
     <h5 class="Media-heading">ID : <a href="#">{{$user->id}}</a></h5>
     <h5 class="Media-heading">First Name : <a href="#">{{$user->first_name}}</a></h5>
     <h5 class="Media-heading">Email : <a href="#">{{$user->email}}</a></h5>
-    <form class="form-inline" action="#">
-      <button class="btn btn-primary btn-block" type="submit">Accept</button>
-      <button class="btn btn-primary btn-block" type="submit">Reject</button>
+    <form class="form-inline" action="{{route('acceptFriend')}}" method="post">
+      <button class="btn btn-success btn-block" type="submit">Accept</button>
+      <input type="hidden" value="{{$search_result->getID()}}" name="id">
+      <input type="hidden" value="{{ Session::token() }}" name="_token">
+    </form>
+    <form class="form-inline" action="{{route('rejectFriend')}}" method="post">
+      <button class="btn btn-danger btn-block" type="submit">Reject</button>
+      <input type="hidden" value="{{$search_result->getID()}}" name="id">
+      <input type="hidden" value="{{ Session::token() }}" name="_token">
     </form>
     <hr>
    </div>
