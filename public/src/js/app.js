58ff0ef1b49adeb468f1ca8d5f483045452ccd92
HiAIDev <asahide0026@gmail.com>
2020-01-17 10:16:31 +0330

    Update app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -14,7 +14,7 @@ $(document).on('click', '#edt', function(event) {
 $("#modal-save").on("click", function() {
     $.ajax({
         method: "POST",
-        url: url,
+        url: urlEdit,
         data: { body: $("#post-body").val(), postId: postId, _token: token }
     }).done(function(msg) {
         $(postBodyElement).text(msg["new_body"]);
